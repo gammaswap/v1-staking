0cfb2902c7038a15c1a0684f8047d65edfa59809
Kalesberg <mobaddicts@hotmail.com>
2023-08-15 11:53:10 -0400

    update hardhat config solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.18",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
