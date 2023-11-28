1633c3276de6d6a246a056bf9e0e00fb1a5ae38c
Kalesberg <mobaddicts@hotmail.com>
2023-11-27 21:28:00 -0500

    update unit tests for reward distributor

--- test/BonusDistributor.test.ts
+++ test/BonusDistributor.test.ts
@@ -95,14 +95,18 @@ describe("BonusDistributor", function () {
 
     await increase(24 * 60 * 60)
 
-    expect(await rewardTracker.claimable(user0.address)).gt(expandDecimals(1785, 18)) // 50000 / 28 => ~1785
-    expect(await rewardTracker.claimable(user0.address)).lt(expandDecimals(1786, 18))
-
     await expect(rewardDistributor.connect(routerAsSigner).setPaused(true))
       .to.emit(rewardDistributor, 'StatusChange')
       .withArgs(rewardTracker.target, anyUint, true);
 
+    let claimable = await rewardTracker.claimable(user0.address);
+    expect(claimable).gt(expandDecimals(1785, 18)) // 50000 / 28 => ~1785
+    expect(claimable).lt(expandDecimals(1786, 18))
+
+    const esGsBalanceBeforeClaim = await esGs.balanceOf(user0.address);
     await rewardTracker.connect(user0).claim(user0.address);
+    const esGsBalanceAfterClaim = await esGs.balanceOf(user0.address);
+    expect(esGsBalanceAfterClaim - esGsBalanceBeforeClaim).eq(claimable);
     expect(await rewardTracker.claimable(user0.address)).eq(0);
 
     await increase(24 * 60 * 60);
@@ -118,5 +122,22 @@ describe("BonusDistributor", function () {
     // Now earning rewards again
     expect(await rewardTracker.claimable(user0.address)).gt(expandDecimals(1785, 18))
     expect(await rewardTracker.claimable(user0.address)).lt(expandDecimals(1786, 18))
+
+    await rewardDistributor.connect(routerAsSigner).setPaused(true);
+
+    await increase(24 * 60 * 60);
+
+    expect(await rewardTracker.claimable(user0.address)).gt(expandDecimals(1785, 18))
+    expect(await rewardTracker.claimable(user0.address)).lt(expandDecimals(1786, 18))
+
+    await rewardDistributor.connect(routerAsSigner).setPaused(false);
+
+    expect(await rewardTracker.claimable(user0.address)).gt(expandDecimals(1785, 18))
+    expect(await rewardTracker.claimable(user0.address)).lt(expandDecimals(1786, 18))
+
+    await increase(24 * 60 * 60);
+
+    expect(await rewardTracker.claimable(user0.address)).gt(expandDecimals(3570, 18))
+    expect(await rewardTracker.claimable(user0.address)).lt(expandDecimals(3572, 18))
   })
 })
