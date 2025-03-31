79bdd892c3b07fcb6d96c9d037bf0a3deba8de74
danielalcarraz <dam417@nyu.edu>
2025-03-31 14:29:23 -0400

    create IEsToken interface

---
+++ contracts/interfaces/IEsToken.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import "./IRestrictedToken.sol";
+
+interface IEsToken is IRestrictedToken {
+    function claimableToken() external view returns(address);
+}
--- contracts/tokens/EsToken.sol
+++ contracts/tokens/EsToken.sol
@@ -2,7 +2,14 @@
 pragma solidity 0.8.21;
 
 import "./RestrictedToken.sol";
+import "../interfaces/IEsToken.sol";
 
-contract EsToken is RestrictedToken {
-    constructor(string memory _name, string memory _symbol) RestrictedToken(_name, _symbol, TokenType.ESCROW) {}
+contract EsToken is RestrictedToken, IEsToken {
+
+    address public immutable override claimableToken;
+
+    constructor(string memory _name, string memory _symbol, address _claimableToken) RestrictedToken(_name, _symbol,
+        TokenType.ESCROW) {
+        claimableToken = _claimableToken;
+    }
 }
